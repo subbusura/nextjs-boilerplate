dcf96d09572ffad779e9937f67c4c9f2d4d09b37
Subramani Krishnan <subramani.krishnan@gndsolutions.in>
2023-11-14 14:56:49 +0530

    --tailwind form added

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import { ManagedUIContext } from "@/contexts/ui.context";
 import NextAuthProvider from "@/contexts/NextAuthProvider";
 const inter = Inter({ subsets: ["latin"] });
+import "./../globals.css";
 
 export const metadata: Metadata = {
   title: "App",
@@ -16,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="en" className="h-full bg-white">
+      <body className={"h-full " + inter.className}>
         <NextAuthProvider>
           <ManagedUIContext>{children}</ManagedUIContext>
         </NextAuthProvider>
